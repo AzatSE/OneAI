5d6846759a0b1cee084da3be16b629074ac4d5e6
Azat <macbookazat@Azats-MacBook-Pro.local>
2026-03-13 01:13:03 +0500

    removed unnecessary code

--- app/core/security.py
+++ app/core/security.py
@@ -34,39 +34,6 @@ def create_refresh_token(data: dict, expires_delta: timedelta | None = None) -> 
     return encoded_jwt
 
 
-
-# def create_refresh_token(db: SessionDep, expires_delta: timedelta | None = None) -> AuthSessions:
-#     if expires_delta:
-#         expire = datetime.now(timezone.utc) + expires_delta
-#     else:
-#         expire = datetime.now(timezone.utc) + timedelta(days=5)
-#     jti = uuid.uuid4().hex
-#
-#     payload = {
-#         "sub":user.username,
-#         "jti":jti,
-#         "type": "refresh",
-#         "exp": expire,
-#         "iat": datetime.now(timezone.utc),
-#     }
-#
-#     token = jwt.encode(payload, settings.SECRET_KEY, settings.ALGORITHM)
-#
-#     data = AuthSessions(user_id= user_id, jti= jti, expires_at= expire, revoked=False)
-#
-#     db.add(data)
-#     db.commit()
-#     db.refresh(data)
-#
-#     return token + data
-
-
-
-
-
-
-
-
 def verify_password(plain_password: str, hashed_password: str):
     return password_hash.verify(plain_password,hashed_password)
 
